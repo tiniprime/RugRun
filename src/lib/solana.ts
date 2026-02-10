ba411b343df0513b175e39b09030eefe0b6619ec
tiniprime <altinkalludracs@gmail.com>
2026-02-10 23:28:59 +0100

    next

--- src/lib/solana.ts
+++ src/lib/solana.ts
@@ -1,9 +1,10 @@
 import { clusterApiUrl } from "@solana/web3.js";
 
-export const TOKEN_MINT = "PASTE_TOKEN_ADDRESS_HERE";
+export const TOKEN_MINT = "BxThE9mZyYCgHuaCSKw5Az4pUzhspUcG8fiYr4AWpump";
 
 export const SOLSCAN_TOKEN_URL = `https://solscan.io/token/${TOKEN_MINT}`;
 export const DEXSCREENER_URL = `https://dexscreener.com/solana/${TOKEN_MINT}`;
+export const X_COMMUNITY_URL = "https://x.com/i/communities/2021322872828878918";
 
 export function getRpcEndpoint(): string {
   return import.meta.env.VITE_SOLANA_RPC || clusterApiUrl("mainnet-beta");
